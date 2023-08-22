e7d86b57b84def624e1784bc27af43b75a67f91f
Andrey Osipov <mitol5549@gmail.com>
2023-08-22 16:34:04 +0200

    Navigating programmatically added

--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -5,9 +5,18 @@ const ClientsProjectsPage = () => {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[projectid]',
+      query: { id: 'max', clientprojectid: 'projecta' },
+    });
+  };
+
   return (
     <>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </>
   );
 };
